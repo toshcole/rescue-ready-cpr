d487d850ab2ec3b946a5e3cce4432e92773f1b46
toshcole <latosha.cole@gmail.com>
2026-02-27 17:21:30 -0500

    Add files via upload

---
+++ layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Rescue Ready CPR & First Aid",
+  description:
+    "CPR, BLS, First Aid, Pet CPR, Stop the Bleed, and on-site group training in Fayetteville and surrounding areas.",
+};
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
